8bf0d460f10daac072de68a046606d173a0c8bfe
Leo Shestakov <you@example.com>
2019-11-18 19:23:52 -0500

    finished to new requirements

--- main.c
+++ main.c
@@ -1,37 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
 
-int main() {
-	struct DIR *direct = opendir(".");
+int main (int argc, char *argv[]) {
+	char toOpen[1000] = ".";
+	if (argc == 1) {
+		fgets(toOpen, sizeof(toOpen), stdin);
+		toOpen[strlen(toOpen) - 1] = '\0';
+	}
+	else {
+		strcpy(toOpen, argv[1]);
+	}
+	struct DIR *direct = opendir(toOpen);
 	long size = 0;
-	
 	if (direct < 0) {
-		printf("Error Opening Assignment Directory: %s\n", strerror(errno));
+		printf("Error Opening Directory: %s\n", strerror(errno));
 		return 0;
 	}
 	struct dirent *file;
-	printf("-----Examining Current Assignment Directory-----\n");
+	printf("----- Examining Directory: [%s] -----\n", toOpen);
 	while ((file = readdir(direct)) != NULL) {
 		int dirCheck = file->d_type;
 		if (dirCheck == 4) {
-			printf("[dir]");
+			printf("[DIR] ");
 		}
 		else {
-			printf("[---]");
+			printf("[---] ");
 		}
 		struct stat fileStats;
-		if (stat(file->d_name, &fileStats) < 0) {
-			printf("Error w/ stat: %s\n", strerror(errno));
-			return 0;
-		}
+		stat(file->d_name, &fileStats);
 		size += fileStats.st_size;
-		printf(" : %s\n", file->d_name);
+		printf(": %s\n", file->d_name);
 	}
-	printf("TOTAL SIZE: %ld bytes\n", size);
-	printf("*** NOTE: [dir] = directory, [---] = other\n");
+	printf("TOTAL DIRECTORY SIZE: %ld bytes\n", size);
+	printf("* KEY: [DIR] = directory, [---] = other\n");
 }
